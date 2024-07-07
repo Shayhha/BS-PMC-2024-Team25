abc30d9d0c22f2cbe976765d8d9d9319853d968a
Maxim Subotin <maxim.sub21@gmail.com>
2024-07-07 15:18:42 +0300

    SETUP : test

--- server/test_main.py
+++ server/test_main.py
@@ -2,13 +2,12 @@ import pytest
 from main import add
 
 def test_add_positive_numbers():
-    result = add(2, 3)
-    assert result == 5, f"Expected 5, but got {result}"
+    assert f() == 4
 
-def test_add_negative_numbers():
-    result = add(-1, -1)
-    assert result == -2, f"Expected -2, but got {result}"
+# def test_add_negative_numbers():
+#     result = add(-1, -1)
+#     assert result == -2, f"Expected -2, but got {result}"
 
-def test_add_zero():
-    result = add(0, 0)
-    assert result == 0, f"Expected 0, but got {result}"
+# def test_add_zero():
+#     result = add(0, 0)
+#     assert result == 0, f"Expected 0, but got {result}"
