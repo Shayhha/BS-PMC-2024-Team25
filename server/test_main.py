9266e3cc05b69059b22d7e1fb66872e856d39990
Maxim Subotin <maxim.sub21@gmail.com>
2024-07-07 16:05:08 +0300

    SETUP : test

--- server/test_main.py
+++ server/test_main.py
@@ -4,9 +4,11 @@ from main import add
 def test_add_positive_numbers():
     assert add(2,2) == 4
 
+
 def test_add_negative_numbers():
     result = add(-1, -1)
     assert result == -2
+
 
 def test_add_zero():
     result = add(0, 0)
