8882208c2a509c424a83756f6ee147adb7f27ac5
Shay Hahiashvili <shayhh@outlook.com>
2024-07-11 20:58:44 +0300

    BSPMS2425-10 PART-1: Added SQLHelper class for database functions and added BugFixer class to handle various functions in website. Added user story for changing user information like username, fname and lname.

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 from abc import ABC
@@ -9,6 +9,10 @@ import os
 # initialize app with flask for backend connection with front end react
 app = Flask(__name__)
 cors = CORS(app, origins='*')
+globalUser = None # this is global user for logged in user
+db = None # this is sql database instance for making queries 
+
+# ====================================================SQLHelper-Class================================================== #
 
 # class for handling SQL commands for various features in website
 class SQLHelper(ABC):
@@ -31,38 +35,113 @@ class SQLHelper(ABC):
         if self.connection:
             self.connection.close()
 
+    # method for searching user in db, returns user obj if found else returns none
+    def searchUser(self, email, password):
+        try:
+            query = 'SELECT * FROM Users WHERE email = ? AND password = ?'
+            self.cursor.execute(query, (email, password,))
+            user = self.cursor.fetchone() # use fetchone to find single user 
+            if user: # if user found
+                return User(user[0], user[1], user[2], user[3], user[4], user[5]) # return user object
+            else: # else return none 
+                return None
+        except Exception as e:
+            print(f'Error: {e}')
+            return None
+        
+    # method for updating user userName, fName, lName  in db, returns true or false
+    def updateUserInfo(self, userId, newUserName, newFname, newLname):
+        try:
+            query = 'UPDATE Users SET userName = ?, fname = ?, lname = ? WHERE userId = ?'
+            self.cursor.execute(query, (newUserName, newFname, newLname, userId,))
+            self.connection.commit()  # commit the transaction for update
+            if self.cursor.rowcount > 0:
+                return True
+            else:
+                return False  # user not found
+        except Exception as e:
+            print(f'Error: {e}')
+            return False
+
+# ==================================================================================================================== #
+
+# ======================================================User-Class==================================================== #
+# class that represents user in website
+class User:
+    # constructor of user class
+    def __init__(self, userId, email, userName, fName, lName, userType):
+        self.userId = userId
+        self.email = email
+        self.userName = userName
+        self.fName = fName
+        self.lName = lName
+        self.userType = userType
+
+    # method for getting a dictionary representation of user object
+    def toDict(self):
+        userDict = {
+            'userId': self.userId,
+            'email': self.email,
+            'username': self.userName,
+            'fName': self.fName,
+            'lName': self.lName,
+            'userType': self.userType
+        }
+        return userDict
+
+# ==================================================================================================================== #
+
+# =====================================================BugFixer======================================================= #
+
+# class that includes various fucntions for interacting with db and using various features in website
+class BugFixer(ABC):
+       
+    # function for login of user into the website
+    @app.route('/homepage/login', methods=['POST'])
+    def login():
+        data = request.get_json()
+        user = db.searchUser(data.get('email'), data.get('password'))
+        if user is not None: # if user was found
+            global globalUser 
+            globalUser = user # set globalUser object
+            return jsonify(user.toDict())
+        else: # else user's credentials aren't valid
+             return jsonify({'error': 'Invalid credentials'})
+        
+    # function for changing user's info like userName, fname, lname
+    @app.route('/userSettings/changeUserInfo', methods=['POST'])
+    def changeUserInfo():
+        data = request.get_json()
+        if db.updateUserInfo(globalUser.userId, data.get('newUserName'), data.get('newFname'), data.get('newLname')):
+            return jsonify({'success': 'Changed user info succusfully'})
+        else:
+            return jsonify({'error': 'failed to perform database query'})
+
+# ==================================================================================================================== #
 
 # function that checks database users
 @app.route('/api/users', methods=['GET'])
 def users():
-    sqlHelper = SQLHelper()
-    try:
-        sqlHelper.connect()
-        sqlHelper.cursor.execute('SELECT * FROM users') 
-        users = sqlHelper.cursor.fetchall()
-        print('Connected successfully to database')
-        userList = [dict(zip([column[0] for column in sqlHelper.cursor.description], row)) for row in users]
-        return jsonify(userList)
-    except:
-        sqlHelper.close()
-        return jsonify('Failed to connect to database.')
-
+    db.cursor.execute('SELECT * FROM users') 
+    users = db.cursor.fetchall()
+    userList = [dict(zip([column[0] for column in db.cursor.description], row)) for row in users]
+    return jsonify(userList)
 
 # example function for testing Jenkins
 def add(a, b):
     """Return the sum of a and b."""
     return a + b
 
-
+# ========================================================MAIN======================================================== #
 # running the python backend app
 if __name__ == '__main__':
     # initialize SQL server connection for database functionality
-    SQLDatabase = SQLHelper() 
+    db = SQLHelper() 
     try:
-        #SQLDatabase.connect()
+        db.connect()
         # execute the app and open website
         app.run(debug=True, port=8090)
         # close dabase connection after website closes
-        #SQLDatabase.close() 
+        db.close() 
     except:
         print('Failed to connect to database.')
