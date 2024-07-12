a01ac9beac003856442289f3209908ff885ef391
Shay Hahiashvili <shayhh@outlook.com>
2024-07-12 22:19:29 +0300

    BSPMS2425-31 PART-3: Finished user story for searching bugs, works as intended.

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -6,9 +6,14 @@ import BugItem from './BugItem';
 import axios from 'axios';
 
 function HomePage() {
+    const [searchResult, setSearchResult] = useState("");
 
+
+    const handleSearchChange = (e) => {
+        setSearchResult(e.target.value);
+    };
+    
     const [isPopupVisible, setIsPopupVisible] = useState(false);
-
     const handleImageClick = () => {
         setIsPopupVisible(true);
     };
@@ -96,13 +101,24 @@ function HomePage() {
         }
     };
 
-
+    const handleSearch = async (e) => { 
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:8090/homePage/search', {searchResult : searchResult});
+            setBugArray(response.data);
+            console.log('Data sent successfully:', response.data);
+        }
+        catch (error) {
+            console.error('Error sending data:', error);
+        }
+    };
+    
     return (
         <div className="home_page">
 
         <div className="home_page_search_container">
-            <input type="text" className="home_page_search_input" placeholder="Search..." />
-            <img src={searchIcon} className="home_page_search_icon" alt="Search" />
+            <input type="text" className="home_page_search_input" placeholder="Search..." value={searchResult} onChange={handleSearchChange}/>
+            <img src={searchIcon} className="home_page_search_icon" alt="Search" onClick={handleSearch}/>
         </div>
 
 
