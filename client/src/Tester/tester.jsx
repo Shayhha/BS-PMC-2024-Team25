c97b0c56192d19d208ae283dbe03a9c627c1f360
Alabar10 <119852999+Alabar10@users.noreply.github.com>
2024-07-12 18:24:46 +0300

    register-BSPMS2425-9

---
+++ client/src/Tester/tester.jsx
@@ -0,0 +1,13 @@
+// src/Tester.jsx
+import React from 'react';
+
+function Tester() {
+    return (
+        <div>
+            <h2>Welcome to Tester Page</h2>
+            {/* Add your Tester content here */}
+        </div>
+    );
+}
+
+export default Tester;
